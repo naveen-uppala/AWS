455bbddb696cd56aa88fb3d6e85c8727e59a8a5a
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2023-05-07 23:59:57 +0530

    Update ec2.tf

--- Terraform/tf-ec2/ec2.tf
+++ Terraform/tf-ec2/ec2.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "security_jenkins_grp" {
-  name        = var.security_group
+resource "aws_security_group" "batch13_security_grp" {
+  name        = "Batch-13 Security Group"
   description = "security group for terraform instances"
+  vpc_id = "vpc-0bed319d89e8b77b9"
 
  ingress {
     from_port   = 22
@@ -23,16 +24,17 @@ resource "aws_security_group" "security_jenkins_grp" {
   }
 
   tags= {
-    Name = var.security_group
+    Name = "Batch-13 Security Group"
   }
 }
 
 resource "aws_instance" "myFirstInstance" {
+  subnet_id = var.subnet_id
   ami     = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [var.security_group]
+  security_groups= [ "${aws_security_group.batch13_security_grp.id}" ]
   tags= {
-    Name = var.tag_name
+    Name = "Batch-13 Instance"
   }
 }  
